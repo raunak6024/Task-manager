af250a7ecaaa1589d16d2bee5267fcb722fa4982
raunak6024 <raunak.24bce11152@vitbhopal.ac.in>
2026-03-26 16:12:50 +0530

    added route.js ,control.js files in controllers and router

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express= require('express')
+const app=express()
+
+
+app.get("/hello",(req,res)=>{
+    res.send("Hello user how are you doing")
+})
+
+
+const port =3000
+app.listen(port,console.log(`server is listining on the port${port}`))
