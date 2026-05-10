ce1743b3aa91d654a272e9da9a12ed692eca00bc
raunak6024 <raunak.24bce11152@vitbhopal.ac.in>
2026-05-10 16:17:59 +0530

    added the validations in schema

--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,16 @@
 const mongoose= require('mongoose');
 
 const TaskSchema = new mongoose.Schema({   //creating scema for document
-    name:String ,completed:Boolean
+    name:{
+      type:String,
+      required:[true,'you cant create a task without a name:'],
+      trim:true,
+      maxlength:[20,'name should not be more than 20 chars:']
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 })
 
 module.exports=mongoose.model('Task',TaskSchema) // create model with name task following schema 
